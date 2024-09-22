c7481ee0a7a4586fdddfc3b7c8b715b0aae263a5
SausePip <user19913@rambler.ru>
2024-09-22 19:09:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from fake_m import divide as fd
+from true_m import divide as td
+
+result1 = fd(69, 3)
+result2 = fd(3, 0)
+result3 = td(49, 7)
+result4 = td(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
